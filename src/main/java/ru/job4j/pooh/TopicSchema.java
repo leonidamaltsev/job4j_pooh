d1682d8218a5059ff4ccf3ba57c5dcbf14b64080
leonidamaltsev <daf.dof@yandex.ru>
2024-08-06 17:25:04 +0300

    3. Тестовое задание - проект "Pooh JMS" [#268841]

--- src/main/java/ru/job4j/pooh/TopicSchema.java
+++ src/main/java/ru/job4j/pooh/TopicSchema.java
@@ -1,19 +1,48 @@
 package ru.job4j.pooh;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class TopicSchema implements Schema {
+    private final ConcurrentHashMap<String, CopyOnWriteArrayList<Receiver>> receivers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, BlockingQueue<String>> data = new ConcurrentHashMap<>();
+    private final Condition condition = new Condition();
 
     @Override
     public void addReceiver(Receiver receiver) {
-
+        receivers.putIfAbsent(receiver.name(), new CopyOnWriteArrayList<>());
+        receivers.get(receiver.name()).add(receiver);
+        condition.on();
     }
 
     @Override
     public void publish(Message message) {
-
+        data.putIfAbsent(message.name(), new LinkedBlockingQueue<>());
+        data.get(message.name()).add(message.text());
+        condition.on();
     }
 
     @Override
     public void run() {
-
+        for (String queueKey : receivers.keySet()) {
+            BlockingQueue<String> queue = data.getOrDefault(queueKey, new LinkedBlockingQueue<>());
+            while (!queue.isEmpty()) {
+                var data = queue.poll();
+                if (data != null) {
+                    var receiversByQueue = receivers.get(queueKey);
+                    for (Receiver receiver : receiversByQueue) {
+                        receiver.receive(data);
+                    }
+                }
+            }
+            condition.off();
+            try {
+                condition.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
